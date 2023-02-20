fbaeeacae1d6411ca5738b26bd298befa6161178
yash1th <yash1th77@gmail.com>
2023-02-20 17:52:19 +0530

    commit

---
+++ springBeanLifeCycle/src/main/java/org/tnsif/beanlifecycle/BookExecutor.java
@@ -0,0 +1,23 @@
+//program to demonstrate on Spring bean life-cycle method
+//driver class
+package org.tnsif.beanlifecycle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BookExecutor {
+
+	public static void main(String[] args) {
+		//ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		AbstractApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+
+		Book b=c.getBean("book",Book.class);
+		System.out.println(b);
+		//used to destroy method of spring bean life cycle
+		/*registerShitdownHook() method comes under the 
+		AbstractApplicationContext*/
+		c.registerShutdownHook();
+		
+	}
+
+}
